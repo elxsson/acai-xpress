b689ccb36d102acd71254da2eebb1af0dfc1aa68
elxsson <soullcakezz@gmail.com>
2024-11-24 22:47:32 -0300

    feat: adiciona CommentController

--- backend/app/Models/Comment.php
+++ backend/app/Models/Comment.php
@@ -15,4 +15,9 @@ class Comment extends Model
     {
         return $this->belongsTo(User::class);
     }
+    
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
 }
